843707e39ea1c74c83faadffb63d7ab668ea91d4
Eltrac Koalar <hi@guhub.cn>
2022-08-25 21:18:51 +0800

    :bowling: Perf: 调整代码解析顺序

--- Plugin.php
+++ Plugin.php
@@ -114,13 +114,13 @@ class BracketDown_Plugin implements Typecho_Plugin_Interface
         $text = empty($last) ? $data : $last;
         if ($widget instanceof Widget_Archive) {
 			$text = BracketDown_Parser::linkToContent(
-					BracketDown_Parser::underline(
-					BracketDown_Parser::textColor(
+					BracketDown_Parser::postCard(
 					BracketDown_Parser::block(
 					BracketDown_Parser::details(
+					BracketDown_Parser::textColor(
 					BracketDown_Parser::kbd(
 					BracketDown_Parser::ruby(
-					BracketDown_Parser::postCard($text
+					BracketDown_Parser::underline($text
 				))))))));
         }
         return $text;
